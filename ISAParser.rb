cc3c4e526c25c611322e50e92aab7a94045ebc4b
BruceZh <xzhong86@163.com>
2021-02-20 20:27:45 +0800

    add bitfields in ISAParser

--- ISAParser.rb
+++ ISAParser.rb
@@ -1,15 +1,77 @@
 
 require_relative 'ISADefParser'
 
+class Integer
+  def to_str(fmt)
+    sprintf(fmt, self)
+  end
+end
+
 class ISAParser
+
+  FieldInfo = Struct.new :hi, :lo, :name, :value
+  class FieldInfo
+    def width()      hi - lo + 1 end
+    def bin_str(with_name: nil)
+      if value
+        sprintf "%0*b", width, value
+      elsif name and with_name
+        "<#{width}.#{name}>"
+      else
+        'x' * width
+      end
+    end
+  end
+
+  class BitFields
+    attr_reader :string, :width, :fields
+    def initialize(str)
+      @width = 0
+      @fields = []
+      @string = str
+      init_with_string(str)
+    end
+    def init_with_string(str)
+      ss = StringScanner.new(str)
+      hi = 31
+      while text = ss.scan(/[01]+|<\d+\.[\w]+>/)
+        field, w = nil, 0
+        if text =~ /^[01]+/
+          w = text.length
+          field = FieldInfo.new hi, hi - w + 1, nil, text.to_i(2)
+        elsif text =~ /^<(\d+)\.([\w]+)>/
+          w, name = $1.to_i, $2
+          field = FieldInfo.new hi, hi - w + 1, name, nil
+        else
+          fail
+        end
+        hi = hi - w
+        @width += w
+        @fields << field
+      end
+      fail "bad binary string" if not ss.eos?
+    end
+
+    def get_bit_str(map = nil) # 0101xxxx01x
+      str = fields.map{ |f| f.bin_str }.join('')
+      map ? str.each_char.map{ |c| map[c] || c }.join('') : str
+    end
+  end
 
   class ISAInstInfo
     attr_reader :type, :name, :bin_str
-    attr_reader :c_codes
+    attr_reader :c_codes, :bitfield
     def initialize(t, n, s)
       @type, @name, @bin_str = t, n, s
+      @bitfield = BitFields.new(s)
+      fail if bitfield.width != 32
       @c_codes = {}
     end
+
+    def bit_str(m=nil)  @bitfield.get_bit_str(m) end
+    def mask_val()      bit_str({ '0' => '1', 'x' => '0' }).to_i(2) end
+    def match_val()     bit_str({ '0' => '0', 'x' => '0' }).to_i(2) end
+
     def set_c_code(type, code)
       if @c_codes[type]
         fail
@@ -22,6 +84,7 @@ class ISAParser
   end
 
   # ========= ISA Hooker =========
+
   class ISAHooker
     attr_reader :insts
     def initialize()
@@ -90,11 +153,15 @@ class ISAParser
     @hook.insts
   end
   def print_inst(inst)
-    puts "#{inst.type} #{inst.name} #{inst.bin_str}"
-    puts "  DECODE: '#{inst.decode_code}'" if inst.decode_code
-    puts "  EXECUTE: '#{inst.execute_code}'" if inst.execute_code
-    puts "  DISASM: '#{inst.disasm_code}'" if inst.disasm_code
-    puts ""
+    bit_str = inst.bit_str
+    msk_str = inst.mask_val.to_str "%08x"
+    val_str = inst.match_val.to_str "%08x"
+    name = "%-8s" % [ inst.name ]
+    puts "#{inst.type} #{name} #{msk_str} #{val_str} #{inst.bin_str}"
+    #puts "  DECODE: '#{inst.decode_code}'" if inst.decode_code
+    #puts "  EXECUTE: '#{inst.execute_code}'" if inst.execute_code
+    #puts "  DISASM: '#{inst.disasm_code}'" if inst.disasm_code
+    #puts ""
   end
 end
 
