66003576837d39feef7dc097a944f32e191551ba
BruceZ <xzhong86@163.com>
2021-03-16 23:08:49 +0800

    fix for riscv isa-def patch

--- test.rb
+++ test.rb
@@ -2,8 +2,9 @@
 require_relative 'ISAParser'
 
 # main
+riscv_isa_def = '../../arch/riscv/isa-def/'
 parser = ISAParser.new
-parser.scan_file('../op-class.def')
-parser.scan_file('../op-01-rv32i.def')
+parser.scan_file(riscv_isa_def + 'op-class.def')
+parser.scan_file(riscv_isa_def + 'op-01-rv32i.def')
 
 parser.insts.each{ |i| parser.print_inst(i) }
